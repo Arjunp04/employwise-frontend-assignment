f5645ecf6373cf982c5701001a534838f4e96eb1
Arjun Ramesh Prajapati <arjunprajapati4102001@gmail.com>
2025-04-10 03:10:18 +0530

    lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,65 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Route, Routes, useLocation } from "react-router";
-import Login from "./pages/Login";
-import Users from "./pages/Users";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
 import { useAuth } from "./context/AuthContext";
 import ProtectedRoute from "./utils/ProtectedRoute";
-import EditUser from "./pages/EditUser";
+
+// Lazy loaded components
+const Login = lazy(() => import("./pages/Login"));
+const Home = lazy(() => import("./pages/Home"));
+const Users = lazy(() => import("./pages/Users"));
+const EditUser = lazy(() => import("./pages/EditUser"));
+const Navbar = lazy(() => import("./components/Navbar"));
 
 const App = () => {
   const location = useLocation();
   const { token } = useAuth();
 
-  // Show Navbar only if logged in and not on "/" or "/login"
   const showNavbar =
     token && location.pathname !== "/" && location.pathname !== "/login";
 
   return (
     <>
-      {showNavbar && <Navbar />}
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route
-          path="users"
-          element={
-            <ProtectedRoute>
-              <Users />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="user/edit/:id"
-          element={
-            <ProtectedRoute>
-              <EditUser />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<div className="text-white p-4">Loading...</div>}>
+        {showNavbar && <Navbar />}
+        <Routes>
+          <Route
+            index
+            element={
+              <Suspense fallback={<div>Loading Home...</div>}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <Suspense fallback={<div>Loading Login...</div>}>
+                <Login />
+              </Suspense>
+            }
+          />
+          <Route
+            path="users"
+            element={
+              <ProtectedRoute>
+                <Suspense fallback={<div>Loading Users...</div>}>
+                  <Users />
+                </Suspense>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="user/edit/:id"
+            element={
+              <ProtectedRoute>
+                <Suspense fallback={<div>Loading Edit Page...</div>}>
+                  <EditUser />
+                </Suspense>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
     </>
   );
 };
